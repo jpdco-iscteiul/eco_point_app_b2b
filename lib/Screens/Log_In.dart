d7462aa42865a5d01cf4fa58bea74ab98e843c3c
jpdco <jpdco@iscte-iul.pt>
2021-04-16 23:53:42 +0100

    Server connection

---
+++ lib/Screens/Log_In.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class Log_In extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => Log_In_state();
+
+}
+
+class Log_In_state extends State<Log_In>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+}
---
+++ lib/constants.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF4C9A25);
+const kPrimaryLightColor= Color(0xFFA8E24C);
+const kContrastColor = Color(0xFF2B2B2B);
+const kContrastLighterColor = Color(0xFFF2F2F2);
+
+String kParseApplicationId= "2Uo1ieG4kQ6FGVJscXUDDibO4wZRz9R12nQ2GBji";
+String kParseClientKey = "n1TjED4DzirLAyYhWtS16wsSm9MolDKIyU5vfvnu";
+String kParseMasterKey = "IotbCAOvEzgVKs6SZwaaO40UEB045o2LNxMQpwgn";
+String kUrl= "https://parseapi.back4app.com/";
